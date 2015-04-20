6198dc4a5ff9e9201eb0b86edb1fbf4a5e97e936
JustinHanssen <justin.hanssen@gmail.com>
2015-04-20 13:13:36 -0400

    Started menu

--- DndCompare.py
+++ DndCompare.py
@@ -1,8 +1,22 @@
 #D&D 5 Weapon Compare
-import random
+#import random
 
 def menu():
-    pass
+    try:
+        nwpns = int(input("How many weapons do you want to compare?\n"))
+        print('Will test {} weapons.\n'.format(nwpns))
+        wlist = []
+        for x in range(1,nwpns+1):
+            wpn = "wpn"+ str(x)
+            print(wpn)
+            wlist.append(wpn)
+            print(wlist)
+    except:
+        ValueError
+        print("You've entered and invalid value.")
+        menu()
+
+
 
 def weaponrolls():
     pass
@@ -22,27 +36,30 @@ class weapon():
     def damage():
         pass
 
+
+menu()
+
 # Concept Test
-weapon1 = weapon('Great Club',7,5,1,8)
+# weapon1 = weapon('Great Club',7,5,1,8)
 
-target = 15
-times = 100000
-dcount = 0
-numhits = 0
-misscount = 0
+# target = 15
+# times = 100000
+# dcount = 0
+# numhits = 0
+# misscount = 0
 
-for n in range(1,times):
-    roll = random.randrange(1, 21) + weapon1.hit
-    if roll >= target:
-        damage = (weapon1.numdice * random.randrange(1, weapon1.sides)) + weapon1.dam
-        dcount = dcount + damage
-        numhits += 1
-    else:
-        misscount += 1
+# for n in range(1,times):
+#     roll = random.randrange(1, 21) + weapon1.hit
+#     if roll >= target:
+#         damage = (weapon1.numdice * random.randrange(1, weapon1.sides)) + weapon1.dam
+#         dcount = dcount + damage
+#         numhits += 1
+#     else:
+#         misscount += 1
 
-avgdam = round((dcount / numhits), 3)
-avgmis = round((misscount / times), 3)* 100
-print("Against an AC of {}".format(target))
-print("The {} did an average of {} points of damage.".format(weapon1.wtype, avgdam))
-print("The {} missed {} percent of the time.".format(weapon1.wtype, avgmis))
-print("")
+# avgdam = round((dcount / numhits), 3)
+# avgmis = round((misscount / times), 3)* 100
+# print("Against an AC of {}".format(target))
+# print("The {} did an average of {} points of damage.".format(weapon1.wtype, avgdam))
+# print("The {} missed {} percent of the time.".format(weapon1.wtype, avgmis))
+# print("")
